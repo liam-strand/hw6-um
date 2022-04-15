f4a949a5d7af57c600d8af7651e9c9e7941d4158
Matt P. Ung <mung01@vm-hw05.eecs.tufts.edu>
2022-04-15 11:29:00 -0400

    Documentation Instruction IP

--- um-code/instructions.c
+++ um-code/instructions.c
@@ -23,77 +23,74 @@
 
 #include "instructions.h"
 
-/* 
- * 
+/* Conditonal Move
+ * Uses check to know if to move from one register to another
  * 
  */
 extern void I_c_mov(uint32_t *source, uint32_t *dest, uint32_t *check)
 {
-    assert(source != NULL && dest != NULL && check != NULL);
     if ((*check) != 0) {
         *dest = *source;
     }
 }
 
-/* 
- * 
+/* Segment load
+ * Moves instruction value from a segment to a register
  * 
  */
 extern void I_seg_load(uint32_t *source, uint32_t *dest)
 {
-    assert(source != NULL && dest != NULL);
     *dest = *source;
 }
 
-/* 
- * 
+/* Segmented Store
+ * Moves 32 bit number to a section of segment
  * 
  */
 extern void I_seg_store(uint32_t *source, uint32_t *dest)
 {
-    assert(source != NULL && dest != NULL);
     *dest = *source;
 }
 
-/* 
- * 
- * 
- */
-extern void I_add(uint32_t *reg_a, uint32_t *reg_b, uint32_t *dest)
-{
-    *dest = *reg_a + *reg_b;
-}
-
-/* 
- * 
+/* Addition
+ * Adds the values of two registers and pushes to desired register
  * 
  */
-extern void I_mult(uint32_t *reg_a, uint32_t *reg_b, uint32_t *dest)
+extern void I_add(uint32_t *reg_b, uint32_t *reg_c, uint32_t *dest)
 {
-    *dest = ((*reg_a) * (*reg_b));
+    *dest = *reg_b + *reg_c;
 }
 
-/* 
- * 
+/* Multilication
+ * Multiplies the values of two registers and pushes to desired register
  * 
  */
-extern void I_div(uint32_t *reg_a, uint32_t *reg_b, uint32_t *dest)
+extern void I_mult(uint32_t *reg_b, uint32_t *reg_c, uint32_t *dest)
 {
-    *dest = ((*reg_a) / (*reg_b));
+    *dest = ((*reg_b) * (*reg_c));
 }
 
-/* 
- * 
+/* Divides
+ * Divides the values of two registers and pushes to desired register
  * 
  */
-extern void I_nand(uint32_t *reg_a, uint32_t *reg_b, uint32_t *dest)
+extern void I_div(uint32_t *reg_b, uint32_t *reg_c, uint32_t *dest)
 {
-    *dest = ~((*reg_a) & (*reg_b));
+    *dest = ((*reg_b) / (*reg_c));
 }
 
-/* 
+/* Bitwise NAND
+ * Nands the values of two registers and pushes to desired register
  * 
- * 
+ */
+extern void I_nand(uint32_t *reg_b, uint32_t *reg_c, uint32_t *dest)
+{
+    *dest = ~((*reg_b) & (*reg_c));
+}
+
+/* Map
+ * Creates a new segment and utilizes recycling older (unmapped) segments
+ * to maintain neccessary memory use and pushes index to desired register
  */
 extern void I_map(Seq_T     other_segs, 
                   Seq_T     available_indices, 
@@ -114,9 +111,9 @@ extern void I_map(Seq_T     other_segs,
     }
 }
 
-/* 
- * 
- * 
+/* Unmap
+ * Removes desired segment and pushes index to available_indices for reuse
+ * to maintain neccessary memory use
  */
 extern void I_unmap(Seq_T     other_segs, 
                     Seq_T     available_indices, 
@@ -129,7 +126,7 @@ extern void I_unmap(Seq_T     other_segs,
     Seq_addhi(available_indices, free_index);
 }
 
-/* 
+/* Output
  * 
  * 
  */
@@ -163,13 +160,9 @@ extern void I_load_p(uint32_t **prog_seg_p, Seq_T   oth_segs, uint32_t *reg_b,
         for (int i = 0; i < len; i++) {
             new_prog[i] = *(uint32_t *) UArray_at(to_copy, i);
         }
-        // fprintf(stderr, "prog_seg1: %p\n", (void *)*prog_seg_p);
         *prog_seg_p = new_prog;
-        // fprintf(stderr, "prog_seg2: %p\n", (void *)*prog_seg_p);
-        // fprintf(stderr, "heyo\n");
     }
     *p_counter = *reg_c;
-    // fprintf(stderr, "reg_c: %d\n", *reg_c);
 }
 
 /* 
--- um-code/instructions.h
+++ um-code/instructions.h
@@ -19,134 +19,184 @@
 #include <stdint.h>
 #include <seq.h>
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Condtional Move
+ *    Purpose: Does a move from source register to destination given the check
+ *             register does not equal 0
+ * Parameters: Pointers too...
+ *               - source register ($r[B])
+ *               - destination register ($r[A])
+ *               - register to check ($r[C] against 0)
  *    Returns: none
- *    Effects: none
+ *    Effects: Shifts values between registers given a condition
  *       CREs: none
  *      Notes: none
  */
 extern void I_c_mov(uint32_t *source, uint32_t *dest, uint32_t *check);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Segment Load
+ *    Purpose: Does a move from source segment section to destination register
+ * Parameters: Pointers too...
+ *               - source (pointer to segment section $m[$r[B]$r[C]])
+ *               - destination register ($r[A])
  *    Returns: none
- *    Effects: none
+ *    Effects: Shifts value from segment section to register
  *       CREs: none
  *      Notes: none
  */
 extern void I_seg_load(uint32_t *source, uint32_t *dest);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Segment Store
+ *    Purpose: Does a move from source register to destination segment section
+ * Parameters: Pointers too...
+ *               - source ($r[C])
+ *               - destination (pointer to segment section $m[$r[A]$r[B]])
  *    Returns: none
- *    Effects: none
+ *    Effects: Shifts value from register to segment section
  *       CREs: none
  *      Notes: none
  */
 extern void I_seg_store(uint32_t *source, uint32_t *dest);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Addition
+ *    Purpose: Adds the values between two registers and puts in destination
+ *             register
+ * Parameters: Pointers too...
+ *               - register B
+ *               - register C 
+ *               - destination register
  *    Returns: none
- *    Effects: none
+ *    Effects: Adds register b to c and pastes to destination register
  *       CREs: none
  *      Notes: none
  */
-extern void I_add(uint32_t *reg_a, uint32_t *reg_b, uint32_t *dest);
+extern void I_add(uint32_t *reg_b, uint32_t *reg_c, uint32_t *dest);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Multiplication
+ *    Purpose: Multiplies the values between two registers and puts in destination
+ *             register
+ * Parameters: Pointers too...
+ *               - register B
+ *               - register C 
+ *               - destination register
  *    Returns: none
- *    Effects: none
+ *    Effects: Multiplies register b to c and pastes to destination register
  *       CREs: none
  *      Notes: none
  */
-extern void I_mult(uint32_t *reg_a, uint32_t *reg_b, uint32_t *dest);
+extern void I_mult(uint32_t *reg_b, uint32_t *reg_c, uint32_t *dest);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Division
+ *    Purpose: Divides the values between two registers and puts in destination
+ *             register
+ * Parameters: Pointers too...
+ *               - register B
+ *               - register C 
+ *               - destination register
  *    Returns: none
- *    Effects: none
+ *    Effects: Divides register b to c and pastes to destination register
  *       CREs: none
  *      Notes: none
  */
-extern void I_div(uint32_t *reg_a, uint32_t *reg_b, uint32_t *dest);
+extern void I_div(uint32_t *reg_b, uint32_t *reg_c, uint32_t *dest);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Bitwise NAND
+ *    Purpose: Does the bitwise nand operation between register C and register
+ *             B and puts it into register A
+ * Parameters: Pointers too...
+ *               - register B
+ *               - register C 
+ *               - destination register
  *    Returns: none
- *    Effects: none
+ *    Effects: Nands register b to c and pastes to destination register
  *       CREs: none
  *      Notes: none
  */
-extern void I_nand(uint32_t *reg_a, uint32_t *reg_b, uint32_t *dest);
+extern void I_nand(uint32_t *reg_b, uint32_t *reg_c, uint32_t *dest);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Map
+ *    Purpose: Creates a UArray and maps it either in an unmapped (recycled) 
+ *             index or a new segment in sequence for non-zero segments
+ *             and pushes the new index to destination register
+ * Parameters: other_segs: sequence representing other segments
+ *             available_indices: sequence representing unmapped segments of
+ *               other_segs to map to first for recycling
+ *             Number of words for new mapped segments
+ *             Pointers too...
+ *               - destination register
  *    Returns: none
- *    Effects: none
+ *    Effects: Adds a segment for access in other_segs
  *       CREs: none
  *      Notes: none
  */
 extern void I_map(Seq_T     other_segs, Seq_T    available_indices, 
                   uint32_t *dest,       uint32_t num_words);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Unmap
+ *    Purpose: Accesses the other segs array and removes a segment indicated by 
+ *             given register and adds this segment to available_indices to
+ *             recycle later
+ * Parameters: other_segs: sequence representing other segments
+ *             available_indices: sequence representing unmapped segments of
+ *               other_segs to map to first for recycling
+ *             Pointers too...
+ *               - source register
  *    Returns: none
- *    Effects: none
+ *    Effects: Removes a segment to make it inaccessible
  *       CREs: none
  *      Notes: none
  */
 extern void I_unmap(Seq_T other_segs, Seq_T     available_indices, 
                                       uint32_t *source);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Output
+ *    Purpose: Prints to standard out the value in given register
+ * Parameters: Pointers too...
+ *               - source register
  *    Returns: none
- *    Effects: none
- *       CREs: none
+ *    Effects: Prints register to stdout given valid char
+ *       CREs: If the register holds a value that is out of range (0, 255)
  *      Notes: none
  */
 extern void I_out(uint32_t *reg);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Input
+ *    Purpose: Takes in from standard in and pushes value into given register
+ * Parameters: Pointers too...
+ *               - destination register
  *    Returns: none
- *    Effects: none
+ *    Effects: Changes a register to hold value from stdin
  *       CREs: none
  *      Notes: none
  */
 extern void I_in(uint32_t *reg);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Load Program
+ *    Purpose: Duplicates $m[$r[B]] (segment indicated by reg B) and 
+ *             replaces $m[0] with duplicated segment. Shifts program 
+ *             to point to the segment zero at instruction number reg C
+ * Parameters: other_segs: sequence representing other segments
+ *             Pointers too...
+ *               - register B
+ *               - register C
+ *               - C-array zero program segment
+ *               - program counter
  *    Returns: none
- *    Effects: none
+ *    Effects: Performs a jump and potentially changes the program segment 
+ *             depending on value in register B since that segment replaces
+ *             zero segment. Potentially does not duplicate if $r[B] is zero
  *       CREs: none
  *      Notes: none
  */
 extern void I_load_p(uint32_t **prog_seg_p, Seq_T   oth_segs, uint32_t *reg_b, 
                      uint32_t  *reg_c,      size_t *p_counter);
 
-/* 
- *    Purpose: 
- * Parameters: none
+/* Load Value
+ *    Purpose: Takes in a value and pushes this to a register
+ * Parameters: value: 32 bit int to push to destination register
+ *             Pointers too...
+ *               - destination register
  *    Returns: none
- *    Effects: none
+ *    Effects: Pushes a given value to register
  *       CREs: none
  *      Notes: none
  */
